c14b1316ef6c241a6705da35eed9a4ea6b94cb8e
Sam James <samjojames@gmail.com>
2023-11-25 15:14:58 +0000

    e2e tests

--- app/page.tsx
+++ app/page.tsx
@@ -38,14 +38,14 @@ export default async function Index() {
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{" "}
+          Built By{" "}
           <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            href="https://github.com/doctone"
             target="_blank"
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            Supabase
+            Sam James
           </a>
         </p>
       </footer>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <h1 className="sr-only">Welcome to Remote Hive</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+      <h1 className="text-3xl">Welcome to Remote Hive</h1>
+      <p className="text-2xl !leading-tight mx-auto max-w-xl text-center">
         Discover new places to work remotely in your local area{" "}
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
