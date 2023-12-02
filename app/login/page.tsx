7907395bf5270ec08af80d722ff9bd52cd5c3aa0
Sam James <samjojames@gmail.com>
2023-12-02 10:53:42 +0000

    base url rdirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -22,7 +22,7 @@ export default function Login({
     });
 
     if (error) {
-      return redirect("/login?message=Could not authenticate user");
+      return redirect(`/login?message=${JSON.stringify(error, null, 2)}`);
     }
 
     return redirect("/");
@@ -31,15 +31,6 @@ export default function Login({
   const signUp = async (formData: FormData) => {
     "use server";
 
-    const getURL = () => {
-      let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL ??
-        process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-        "http://localhost:3000/";
-      url = url.includes("http") ? url : `https://${url}`;
-      url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-      return url;
-    };
     const origin = headers().get("origin");
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
@@ -50,12 +41,12 @@ export default function Login({
       email,
       password,
       options: {
-        emailRedirectTo: `${getURL()}/auth/callback`,
+        emailRedirectTo: process.env.BASE_URL,
       },
     });
 
     if (error) {
-      return redirect("/login?message=Could not authenticate user");
+      return redirect(`/login?message=${error.message}`);
     }
 
     return redirect("/login?message=Check email to continue sign in process");
