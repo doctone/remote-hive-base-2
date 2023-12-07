5ab1feb73cbbd8c7b0352d2424ad935cede6f1f4
Sam James <samjojames@gmail.com>
2023-12-07 09:39:08 +0000

    upgrade node version on ci

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,7 +3,6 @@ import { afterEach, expect } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
 
-// extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
 
 afterEach(() => {
